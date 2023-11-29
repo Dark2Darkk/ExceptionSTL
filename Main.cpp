b38d121944a09f6bbcd2292b3d84edbdc3fcfc66
Cole <coltoncoey13@gmail.com>
2023-11-29 10:08:19 -0500

    Added invalid range exception

--- Main.cpp
+++ Main.cpp
@@ -11,3 +11,25 @@ public:
 		return "Invalid Character Exception";
 	}
 };
+
+class invalidRangeException : public exception {
+public:
+	const char* what() const throw() {
+		return "Invalid Range Exception";
+	}
+};
+
+char character(char start, int offset) {
+	if (!isalpha(start)) {
+		throw invalidCharacterException();
+	}
+
+	char result = start + offset;
+
+	if (!isalpha(result) || (islower(start) != islower(result))) {
+		throw invalidRangeException();
+	}
+
+	return result;
+}
+
